bbfdb3e8eea79fbe8517c0826b3d26658786a378
Roman <romek20@mail.ru>
2022-12-23 14:41:53 +0300

    Решение задачи 58

---
+++ Task_58/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить
+//  произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+Console.Write("Введите количество строк матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix1 = new int[rows, columns];
+int[,] matrix2 = new int[rows, columns];
+int[,] resultMatrix = new int[rows, columns];
+Random rnd = new Random(); 
+
+Console.WriteLine("Заданная двумерная матрица 1: ");
+FillMatrix(matrix1);
+PrintMatrix(matrix1);
+Console.WriteLine("Заданная двумерная матрица 2: ");
+FillMatrix(matrix2);
+PrintMatrix(matrix2);
+Console.WriteLine($"Произведение двух матриц: ");
+MultiplyMatrix(resultMatrix);
+PrintMatrix(resultMatrix);
+
+void MultiplyMatrix(int[,] matrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < matrix.GetLength(1); k++)
+      {
+        sum += matrix1[i,k] * matrix2[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int[,] FillMatrix(int[,] matrix)
+{  
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
